0cf831466bb3e4e050812bb004255c6cb6f74ad5
Jason Barr <jason@jasonsbarr.com>
2019-11-21 20:22:43 -0600

    Get dev and prod builds working for JS with cleanup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.config.common");
 const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -8,4 +9,9 @@ module.exports = merge(common, {
     filename: "app.bundle.dev.js",
     path: path.resolve(__dirname, "./build"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/public/index.html",
+    }),
+  ]
 });
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const common = require("./webpack.config.common");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -7,4 +10,10 @@ module.exports = merge(common, {
     filename: "[name]-[contentHash].bundle.js",
     path: path.resolve(__dirname, "./build"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ]
 });
