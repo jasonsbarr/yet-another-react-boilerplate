aaee3c3f50b60504b567548c947d3ec2154ada92
Jason Barr <jason@jasonsbarr.com>
2019-11-22 01:51:29 -0600

    Optimize production JS build, fix typo

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -8,22 +8,24 @@ const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const PostCssNormalize = require("postcss-normalize");
 const PostCssFlexbugsFixes = require("postcss-flexbugs-fixes");
 const PostCssPresetEnv = require("postcss-preset-env");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "[name]-[contentHash].bundle.js",
+    chunkFilename: "[name]-[chunkhash].bundle.js",
     path: path.resolve(__dirname, "./build"),
   },
   optimization: {
-    minimizer: [new OptimizeCssAssetsPlugin()],
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/public/index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: "[name]-[contentHast].css",
+      filename: "[name]-[contentHash].css",
       chunkFilename: "[id].css",
     }),
     new CleanWebpackPlugin(),
