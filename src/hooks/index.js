19fd5ddb680cb2af14a5b2d92a8cbabeb8d0f018
Jason Barr <jason@jasonsbarr.com>
2019-11-27 21:28:44 -0600

    Remove reference to non-existent hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,4 @@
 import useLocalStorage from "./useLocalStorage";
 import useFetchGet from "./useFetchGet";
-import useStateProvider from "./useStateProvider";
 
-export { useLocalStorage, useFetchGet, useStateProvider };
+export { useLocalStorage, useFetchGet };
