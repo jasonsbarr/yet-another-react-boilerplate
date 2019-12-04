19bd077a18fe24d4fbf86f67aec6e2441b344b0b
Jason Barr <jason@jasonsbarr.com>
2019-12-03 23:46:34 -0600

    Change .babelrc to babelrc.js for more flexible config

--- babelrc.js
+++ babelrc.js
@@ -1,17 +1,17 @@
-{
-  "presets": [
+module.exports = {
+  presets: [
     "@babel/preset-react",
     "@babel/preset-env",
     "@emotion/babel-preset-css-prop"
   ],
-  "plugins": [
+  plugins: [
     [
       "@babel/plugin-proposal-decorators",
       {
-        "decoratorsBeforeExport": true
+        decoratorsBeforeExport: true
       }
     ],
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-syntax-bigint"
   ]
-}
+};
