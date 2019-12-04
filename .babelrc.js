9d38164b8a5cc15beee8091709a774b40a094d0f
Jason Barr <jason@jasonsbarr.com>
2019-12-03 23:55:27 -0600

    Fix typo & change module transform in test env so tree shaking still works

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,14 @@
+const isTest = String(process.env.NODE_ENV) === "test";
+
 module.exports = {
   presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: isTest ? "commonjs" : false
+      }
+    ],
     "@babel/preset-react",
-    "@babel/preset-env",
     "@emotion/babel-preset-css-prop"
   ],
   plugins: [
