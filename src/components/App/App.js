76d984ea6be899ee6e377171800937205cca007e
Jason Barr <jason@jasonsbarr.com>
2019-12-03 23:57:34 -0600

    Add Jest to pre-commit hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes } from "../../routes";
 import "./App.scss";
 
+// comment for pre-commit hook testing purposes
+
 const App = () => (
   <div className="App">
     <Routes />
